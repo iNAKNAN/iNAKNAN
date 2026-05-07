bd07e035745198cd469524e09c6782b2ed3cce5e
HandPump <guramaipedas@gmail.com>
2026-05-07 21:57:23 +0700

    fix(flutter): inisialisasi locale id_ID untuk DateFormat
    - main.dart: tambah initializeDateFormatting('id_ID') sebelum runApp
    - Ini fix gray box / ErrorWidget saat render _DriverLogCard & _HistoryRow

--- ekspedisi_app/lib/main.dart
+++ ekspedisi_app/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
 import 'providers/app_provider.dart';
 import 'theme/app_theme.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id_ID', null);
   runApp(const MyApp());
 }
 
